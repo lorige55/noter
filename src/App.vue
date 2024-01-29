afe5fa1bfdb82223e62ee6f69b300954b1bc25dc
Löris <101009352+lorige55@users.noreply.github.com>
2024-01-29 12:40:30 +0100

    Add Bootstrap icons and implement document deletion and creation

--- src/App.vue
+++ src/App.vue
@@ -28,6 +28,38 @@ export default {
       const db = getFirestore(app)
       let docRef = doc(db, this.authToken, this.activeDocument)
       setDoc(docRef, { note: this.activeDocumentContent })
+    },
+    async deleteDocument(item) {
+      const app = initializeApp(firebaseConfig)
+      const db = getFirestore(app)
+      const indexToRemove = this.noteIndex.indexOf(item)
+
+      if (indexToRemove !== -1) {
+        this.noteIndex.splice(indexToRemove, 1)
+
+        let docRef = doc(db, this.authToken, 'noteIndex')
+        setDoc(docRef, { index: this.noteIndex })
+      }
+
+      console.log(this.noteIndex)
+
+      await deleteDoc(doc(db, this.authToken, item))
+    },
+    createNewDocument() {
+      const app = initializeApp(firebaseConfig)
+      const db = getFirestore(app)
+      const newNoteName = prompt('Enter a name for your new note:')
+
+      this.noteIndex.push(newNoteName)
+
+      let docRef = doc(db, this.authToken, 'noteIndex')
+      setDoc(docRef, { index: this.noteIndex })
+
+      docRef = doc(db, this.authToken, newNoteName)
+      setDoc(docRef, { note: 'This is a note.' })
+
+      this.activeDocumentContent = 'This is a note.'
+      this.activeDocument = newNoteName
     }
   },
   beforeMount() {
@@ -64,7 +96,7 @@ export default {
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
+import { getFirestore, doc, getDoc, setDoc, deleteDoc } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -89,15 +121,26 @@ const firebaseConfig = {
       <ul class="list-group">
         <li class="list-group-item" v-for="item in noteIndex">
           <button class="btn btn-dark" @click="getDocument(item)">{{ item }}</button>
+          <buttton class="btn btn-danger" @click="deleteDocument(item)"
+            ><i class="bi bi-trash3"></i
+          ></buttton>
         </li>
       </ul>
     </div>
 
     <div class="position-absolute top-50 end-0 translate-middle-y">
-      <input class="form-control" type="text" v-model="activeDocumentContent" />
+      <input
+        class="form-control"
+        type="text"
+        v-model="activeDocumentContent"
+        style="width: 1000px; height: 500px"
+      />
     </div>
     <div class="position-absolute top-0 end-0">
       <button class="btn btn-dark" @click="saveActiveDocument()">Save</button>
+      <button class="btn btn-success" @click="createNewDocument()">
+        <i class="bi bi-plus-circle"></i>
+      </button>
     </div>
   </div>
 </template>
