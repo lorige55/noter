7a18122e8f1e92dbb6bb0fb7ba1011b676b60eb8
Löris <101009352+lorige55@users.noreply.github.com>
2024-03-20 14:30:03 +0100

    Updated menubar

--- src/App.vue
+++ src/App.vue
@@ -733,17 +733,11 @@ export default {
             <MenubarMenu>
               <MenubarTrigger>View</MenubarTrigger>
               <MenubarContent>
-                <MenubarCheckboxItem>Always Show Bookmarks Bar</MenubarCheckboxItem>
-                <MenubarCheckboxItem checked> Always Show Full URLs </MenubarCheckboxItem>
-                <MenubarSeparator />
-                <MenubarItem inset> Reload <MenubarShortcut>⌘R</MenubarShortcut> </MenubarItem>
-                <MenubarItem disabled inset>
-                  Force Reload <MenubarShortcut>⇧⌘R</MenubarShortcut>
+                <MenubarItem inset @click="location.reload()">
+                  Reload <MenubarShortcut>⌘R</MenubarShortcut>
                 </MenubarItem>
                 <MenubarSeparator />
                 <MenubarItem inset @click="fullscreen()"> Toggle Fullscreen </MenubarItem>
-                <MenubarSeparator />
-                <MenubarItem inset> Hide Sidebar </MenubarItem>
               </MenubarContent>
             </MenubarMenu>
           </Menubar>
